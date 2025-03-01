240ee6ef1589903aca2213086b17e1b69759229a
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2025-03-01 17:19:09 +0100

    fix rankmirror, formatting

--- install-aur-and-mirror-helpers.sh
+++ install-aur-and-mirror-helpers.sh
@@ -1,45 +1,40 @@
 #! /usr/bin/env bash
 echo "NOW RUNNING: $0 AS $USER"
 source ./common.sh
-if [ -x $(command -v "yay") ]
-then
-    printf "\nmanually install yay\n"
-    # manual
-    aurgitmake_install yay-bin
+if [ -x $(command -v "yay") ]; then
+  printf "\nmanually install yay\n"
+  # manual
+  aurgitmake_install yay-bin
 fi
 
 [ $(command -v "yay") ] &&
-    yay --needed --noconfirm -S pikaur-static
+  yay --needed --noconfirm -S pikaur-static
 
 # PIKAUR
-if [ -x $(command -v "pikaur") ]
-then
-    printf "\nmanually install pikaur\n" 
-    # install 
-    aurgitmake_install pikaur
-    if [ -x $(command -v "pikaur") ]
-    then
-      printf "\try install pikaur-static\n" 
-          # install 
-          aurgitmake_install pikaur-static
-          if [ -x $(command -v "pikaur") ]
-          then
-            printf "\paru install pikaur\n" 
-                # install 
-                sudo pacman -S paru
-                paru -S pikaur-static
-          fi
+if [ -x $(command -v "pikaur") ]; then
+  printf "\nmanually install pikaur\n"
+  # install
+  aurgitmake_install pikaur
+  if [ -x $(command -v "pikaur") ]; then
+    printf "\try install pikaur-static\n"
+    # install
+    aurgitmake_install pikaur-static
+    if [ -x $(command -v "pikaur") ]; then
+      printf "\paru install pikaur\n"
+      # install
+      sudo pacman -S paru
+      paru -S pikaur-static
     fi
+  fi
 
 fi
 
 # rank mirror because pacman-key is slow
-if [ -x $(command -v "rankmirrors") ]
-then
-    printf "\nInstall rankmirrors:\n"
-    install pacman-contrib # >/dev/null 2>&1
-    install rankmirrors-systemd # >/dev/null 2>&1
-    # task todo seems to not get appended
-    rankmirrors /etc/pacman.d/mirrorlist | sudo tee -a /etc/pacman.d/mirrorlist
+if [ -x $(command -v "rankmirrors") ]; then
+  printf "\nInstall rankmirrors:\n"
+  install pacman-contrib      # >/dev/null 2>&1
+  install rankmirrors-systemd # >/dev/null 2>&1
+  # task todo seems to not get appended
+  # rankmirrors /etc/pacman.d/mirrorlist | sudo tee -a /etc/pacman.d/mirrorlist
+  rankmirrors -f 15
 fi
-
